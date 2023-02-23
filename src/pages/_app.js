6628d74c19d124432be4ab5ee84777c00eb13d2b
SaiAdithya3 <saiadithyakancharla@gmail.com>
2023-02-23 16:50:37 +0530

    done

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,37 @@
-import '@/styles/globals.css'
+// import "@picocss/pico";
+import "../styles/globals.css";
+// import "../styles/Home.module.css";
+// import styles from "../styles/globals.css";
+import Link from "next/link";
+import AuthProvider from "../contexts/AuthContext";
+import TransactionProvider from "../contexts/TransactionContext";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+  return (
+    <div>
+      <nav className="container header">
+        <ul>
+          <li>Flow Demo</li>
+        </ul>
+        <ul>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+        </ul>
+      </nav>
+      <main className="container">
+        <TransactionProvider>
+          <AuthProvider>
+            <Component {...pageProps} />
+          </AuthProvider>
+        </TransactionProvider>
+      </main>
+      <footer className="container">
+        
+        
+      </footer>
+    </div>
+  );
 }
+
+export default MyApp;
